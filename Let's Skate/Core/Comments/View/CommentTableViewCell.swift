6b899d8a274e88b2a4c408077c60c15733feed5e
Youssef <youssef.b.air2@gmail.com>
2022-05-15 17:48:40 +0100

    + string date format extension

--- Let's Skate/Core/Comments/View/CommentTableViewCell.swift
+++ Let's Skate/Core/Comments/View/CommentTableViewCell.swift
@@ -116,38 +116,10 @@ class CommentTableViewCell: UITableViewCell {
     func configure(comment: Comment) {
         nicknameLabel.text = comment.user?.nickname
         commentLabel.text = comment.comment
-        let timestamp = getReadableDate(timeStamp: Double(comment.date.seconds))
+        let timestamp = String().getReadableDate(timeStamp: Double(comment.date.seconds))
         datePostedLabel.text = timestamp
         guard let profileImageUrl = comment.user?.profileImageUrl else { return }
         profileImageView.sd_setImage(with: URL(string: profileImageUrl))
-    }
-    
-    func getReadableDate(timeStamp: TimeInterval) -> String? {
-        let date = Date(timeIntervalSince1970: timeStamp)
-        let dateFormatter = DateFormatter()
-        
-        if Calendar.current.isDateInTomorrow(date) {
-            return "Tomorrow"
-        } else if Calendar.current.isDateInYesterday(date) {
-            return "Yesterday"
-        } else if dateFallsInCurrentWeek(date: date) {
-            if Calendar.current.isDateInToday(date) {
-                dateFormatter.dateFormat = "h:mm a"
-                return dateFormatter.string(from: date)
-            } else {
-                dateFormatter.dateFormat = "EEEE"
-                return dateFormatter.string(from: date)
-            }
-        } else {
-            dateFormatter.dateFormat = "MMM d, yyyy"
-            return dateFormatter.string(from: date)
-        }
-    }
-
-    func dateFallsInCurrentWeek(date: Date) -> Bool {
-        let currentWeek = Calendar.current.component(Calendar.Component.weekOfYear, from: Date())
-        let datesWeek = Calendar.current.component(Calendar.Component.weekOfYear, from: date)
-        return (currentWeek == datesWeek)
     }
     
     
--- Let's Skate/Extension/StringVerification.swift
+++ Let's Skate/Extension/StringVerification.swift
@@ -11,23 +11,33 @@ extension String {
     var isOnlyNumberAndLetter: Bool {
         return self.allSatisfy{ $0.isNumber || $0.isLetter }
     }
-}
-
-func convertDateFormatter(date: String) -> String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"//this your string date format
-    dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
-    dateFormatter.locale = Locale(identifier: "your_loc_id")
-    let convertedDate = dateFormatter.date(from: date)
-
-    guard dateFormatter.date(from: date) != nil else {
-        assert(false, "no date from string")
-        return ""
+    
+    func getReadableDate(timeStamp: TimeInterval) -> String? {
+        let date = Date(timeIntervalSince1970: timeStamp)
+        let dateFormatter = DateFormatter()
+        
+        if Calendar.current.isDateInTomorrow(date) {
+            return "Tomorrow"
+        } else if Calendar.current.isDateInYesterday(date) {
+            return "Yesterday"
+        } else if dateFallsInCurrentWeek(date: date) {
+            if Calendar.current.isDateInToday(date) {
+                dateFormatter.dateFormat = "h:mm a"
+                return dateFormatter.string(from: date)
+            } else {
+                dateFormatter.dateFormat = "EEEE"
+                return dateFormatter.string(from: date)
+            }
+        } else {
+            dateFormatter.dateFormat = "MMM d, yyyy"
+            return dateFormatter.string(from: date)
+        }
     }
-
-    dateFormatter.dateFormat = "yyyy MMM HH:mm EEEE"///this is what you want to convert format
-    dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
-    let timeStamp = dateFormatter.string(from: convertedDate!)
-
-    return timeStamp
 }
+
+func dateFallsInCurrentWeek(date: Date) -> Bool {
+    let currentWeek = Calendar.current.component(Calendar.Component.weekOfYear, from: Date())
+    let datesWeek = Calendar.current.component(Calendar.Component.weekOfYear, from: date)
+    return (currentWeek == datesWeek)
+}
+
