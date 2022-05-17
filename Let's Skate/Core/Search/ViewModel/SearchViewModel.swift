71b677f01196c8820c7c8dac40a646245ee7c2ab
Youssef <youssef.b.air2@gmail.com>
2022-05-17 23:44:44 +0100

    + Searching viewModel

--- Let's Skate/Core/Search/ViewModel/SearchViewModel.swift
+++ Let's Skate/Core/Search/ViewModel/SearchViewModel.swift
@@ -7,8 +7,28 @@
 
 import Foundation
 
+protocol searchViewModelOutPut : AnyObject {
+    func updateUsers(users: [User])
+    func showError(error: Error)
+}
+
 class SearchViewModel {
     
+    weak var output: searchViewModelOutPut?
+    let searchService: SearchService
     
+    init(searchService: SearchService) {
+        self.searchService = searchService
+    }
     
+    func seachUserWithUserName(username: String) {
+        searchService.searchUserByUsername(username: username) {[weak self] results in
+            switch results {
+            case .success(let users):
+                self?.output?.updateUsers(users: users)
+            case .failure(let error):
+                self?.output?.showError(error: error)
+            }
+        }
+    }
 }
