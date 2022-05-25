17d4cc7197148ea07428ec787d31d2fa8d1e4d33
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-05-25 21:27:25 +0100

    tableView Setup

--- Let's Skate/Core/Messages/MessagesViewController.swift
+++ Let's Skate/Core/Messages/MessagesViewController.swift
@@ -8,9 +8,52 @@
 import UIKit
 
 final class MessagesViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private let messagesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(MessageTableViewCell.self, forCellReuseIdentifier: MessageTableViewCell.identifier)
+        return tableView
+    }()
+    
+    // MARK: - View Model
+    
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSubviews()
+        setupTableView()
     }
-
+    
+    // MARK: - Set up
+    private func setupSubviews() {
+        view.addSubview(messagesTableView)
+    }
+    
+    private func setupTableView() {
+        messagesTableView.delegate = self
+        messagesTableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        messagesTableView.frame = view.bounds
+    }
+    
+    // MARK: - Functions
+    
+    
+    // MARK: - Network Manager calls
+    
+}
+// MARK: - Extensions
+extension MessagesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MessageTableViewCell.identifier, for: indexPath) as? MessageTableViewCell else { return UITableViewCell() }
+        return cell
+    }
 }
--- Let's Skate/Core/Messages/View/MessageTableViewCell.swift
+++ Let's Skate/Core/Messages/View/MessageTableViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class MessageTableViewCell: UITableViewCell {
     
+    static let identifier = "MessageTableViewCell"
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }
