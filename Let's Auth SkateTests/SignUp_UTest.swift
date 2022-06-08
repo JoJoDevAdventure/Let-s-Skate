5a8e27de66214c2665b3bcedbad10de336911f1e
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-08 23:40:23 +0100

    SignUp Test #1

--- Let's Auth SkateTests/SignUp_UTest.swift
+++ Let's Auth SkateTests/SignUp_UTest.swift
@@ -4,11 +4,22 @@
 //
 //  Created by Youssef Bhl on 27/04/2022.
 //
-
+@testable import Let_s_Skate
 import XCTest
 
 class SignUp_UTest: XCTestCase {
     
+    var signUp: RegistrationService!
+    var user: User?
     
+    override func setUp() {
+        super.setUp()
+        signUp = AuthManager()
+    }
+    
+    override func tearDown() {
+        signUp = nil
+        super.tearDown()
+    }
 
 }
