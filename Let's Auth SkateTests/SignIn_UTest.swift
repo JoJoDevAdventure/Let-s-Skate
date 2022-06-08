ead3cfbbee3bfec0e61a053adda27d4cf69321a6
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-08 23:10:50 +0100

    Login Test #3

--- Let's Auth SkateTests/SignIn_UTest.swift
+++ Let's Auth SkateTests/SignIn_UTest.swift
@@ -49,13 +49,38 @@ class SignIn_UTest: XCTestCase {
             XCTAssert(user == nil)
         } catch {
             // verify that we got the correct error
-            
             XCTAssert(error as! LoginErrors == LoginErrors.FIRAuthErrorCodeInvalidEmail)
         }
     }
     
     func test_email_correct_exist_but_wrong_password() async {
-        
+        do {
+            try await signUp.loginUserWith(email: "youssef.b.air2@gmail.com", password: "1111111")
+            XCTAssert(user == nil)
+        } catch {
+            // verify that we got the correct error
+            XCTAssert(error as! LoginErrors == LoginErrors.FIRAuthErrorCodeWrongPassword)
+        }
+    }
+    
+    func test_login_to_disabled_account() async {
+        do {
+            try await signUp.loginUserWith(email: "jojonoah@gmail.com", password: "12341234")
+            XCTAssert(user == nil)
+        } catch {
+            // verify that we got the correct error
+            print(error)
+            XCTAssert(error as! LoginErrors == LoginErrors.FIRAuthErrorCodeUserDisabled)
+        }
+    }
+    
+    // Turn of mac connection
+    func test_no_connection_while_login() async {
+        do {
+            try await signUp.loginUserWith(email: "youssef.b.air2@gmail.com", password: "12341234")
+        } catch {
+            XCTAssert(error as! LoginErrors == LoginErrors.FIRAuthErrorCodeUnkown)
+        }
     }
     
     func test_email_is_valid_and_correct_should_login() async {
